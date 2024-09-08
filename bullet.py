0c279d0566f7541f831d9900d5089d42af0bac15
brunsilva-jc <brunsilva.jc@gmail.com>
2024-09-08 10:56:26 -0300

    created new type of bullet

--- bullet.py
+++ bullet.py
@@ -4,18 +4,18 @@ from pygame.sprite import Sprite
 class Bullet(Sprite):
     """ class to manage bullets fired from the warship """
 
-    def __init__(self, eafm_settings, screen, warship):
+    def __init__(self, bullet_width, bullet_height, bullet_color, bullet_speed, screen, warship):
         super(Bullet, self).__init__()
         self.screen = screen
 
-        self.rect = pygame.Rect(0, 0, eafm_settings.bullet_width, eafm_settings.bullet_height)
+        self.rect = pygame.Rect(0, 0, bullet_width, bullet_height)
         self.rect.centerx = warship.rect.centerx
         self.rect.top = warship.rect.top
 
         self.bullet_position = float(self.rect.y)
 
-        self.color = eafm_settings.bullet_color
-        self.bullet_speed = eafm_settings.bullet_speed
+        self.color = bullet_color
+        self.bullet_speed = bullet_speed
 
     def update(self):
         """ Moving the bullet on the screen """
