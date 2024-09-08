4ed003d3a966a560b6041229952ab1bdce3e5303
brunsilva-jc <brunsilva.jc@gmail.com>
2024-09-08 10:55:43 -0300

    new function to check screen edges and aliens position

--- alien.py
+++ alien.py
@@ -6,6 +6,7 @@ class Alien(Sprite):
 
     def __init__(self, eafm_settings, screen):
         super(Alien, self).__init__()
+        self.x = None
         self.screen = screen
         self.eafm_settings = eafm_settings
 
@@ -17,6 +18,17 @@ class Alien(Sprite):
 
         self.alien_position = float(self.rect.x)
 
+    def check_edges(self):
+        """ return true if alien hit the screen edge """
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
+
+    def update(self):
+        self.x += self.eafm_settings.alien_speed_factor * self.eafm_settings.fleet_direction
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
